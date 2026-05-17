be6459c75c355f12267a1655025796beed422f36
gaurav mishra <gauravmishra7913@gmail.com>
2026-05-17 20:24:51 +0530

    render deploye

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   preview: {
     host: true,
-    allowedHosts: ["aijobplatform-1.onrender.com"],
+    allowedHosts: ["aijobplatform-1.onrender.com", "aijobplatform-3.onrender.com"],
   },
 });
